fdcbc0ea00d2ccbd1bb371ddf302842f79a079be
JonathanMaillard <jonathan.maillard95@gmail.com>
2021-01-25 11:20:07 +0100

    better error handling

--- index.js
+++ index.js
@@ -5,17 +5,24 @@ const axios = require('axios');
 
 
 
-//get the country sent in arguments and get the country code
-let myArgs = process.argv.slice(2);
-let country = myArgs[0];
+//create a table with the arguments
+const myArgs = process.argv.slice(2);
 
+// check the number of argument and abort if different than 1 or 2
+if(myArgs.length != 2 && myArgs.length != 1){
+    console.log("Synthax Error. You must declare a Country and, optionnaly, a date.");
+    return;
+}
+
+//get the country and its code
+let country = myArgs[0];
 let code = getCode(country);
+
+//get the current yeah
 let year = new Date().getFullYear();
 
-// count number of arguments
-let argNumber = process.argv.length;
 // if a year is passed as an argument, use it instead of the current year
-if(argNumber > 3){
+if(myArgs.length == 2){
     year = Number(myArgs[1]);
 }
 
@@ -31,6 +38,7 @@ axios.get('https://date.nager.at/api/v2/PublicHolidays/'+year+'/'+code)
   })
   .catch(function (error) {
     // handle error
-    console.log(error);
+    //console.log(error);
+    console.error('You migh have entered a wrong Country name or an invalid year');
   })
   
